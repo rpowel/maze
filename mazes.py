5e04ba278edd3f494beaaed143d1da1c0b6840dc
Robert Powel <robbie.powel@gmail.com>
2020-11-19 08:26:05 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   MazeGui.ui
    	modified:   __pycache__/mazegui.cpython-38.pyc
    	modified:   mazegui.py
    	modified:   mazes.py
    
     Untracked files:
    	__pycache__/mazes.cpython-38.pyc
    	app.py

--- mazes.py
+++ mazes.py
@@ -5,22 +5,10 @@ Created on Wed Nov 11 10:44:44 2020.
 
 @author: powel
 """
-import sys
 import numpy as np
 from scipy import spatial
-from PyQt5 import QtWidgets, QtGui
-from mazegui import Ui_mazeMenu
-import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.pyplot import pcolor
 import random
-
-
-class Colors:
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
 
 
 class Maze:
@@ -297,59 +285,7 @@ class KruskalMaze:
         self.maze = maze_temp
 
 
-class App():
-    def __init__(self):
-        self._initWindow()
-        self._ax = None
-
-        self._mazeSelectButtons = [
-            self.ui.randomButton,
-            self.ui.primButton,
-            self.ui.kuzatsButton
-            ]
-
-        self.ui.drawButton.clicked.connect(self._drawMaze)
-
-        self._exit()
-
-    def _initWindow(self):
-        self.app = QtWidgets.QApplication(sys.argv)
-        self.mazeMenu = QtWidgets.QDialog()
-        self.ui = Ui_mazeMenu()
-        self.ui.setupUi(self.mazeMenu)
-        self.mazeMenu.show()
-
-    def _exit(self):
-        sys.exit(self.app.exec_())
-
-    def _getChoice(self):
-        for button in self._mazeSelectButtons:
-            if button.isChecked():
-                choice = button.text()
-                return choice
-
-    def _getDimensions(self):
-        nX = self.ui.nXBox.value()
-        nY = self.ui.nYBox.value()
-        return nX, nY
-
-    def _makeMaze(self):
-        choice = self._getChoice()
-        nX, nY = self._getDimensions()
-        maze = Maze().make_maze(nX, nY, maze_type=choice)
-        return maze
-
-    def _drawMaze(self):
-        maze = self._makeMaze()
-        canvas = FigureCanvas(Figure())
-        if self._ax is None:
-            self.ui.verticalLayout_3.addWidget(canvas)
-        else:
-            self._ax = None
-        self._ax = canvas.figure.subplots()
-        self._ax.pcolor(maze)
-        self._ax.axis('off')
-
-
-if __name__ == '__main__':
-    App()
+if __name__ == "__main__":
+    n_x = n_y = 8
+    maze = Maze().make_maze(n_x, n_y, maze_type='Prim')
+    print(maze)
