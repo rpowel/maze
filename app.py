34500f347c54724c14a525f1f8aac65967bf0e16
Robert Powel <robbie.powel@gmail.com>
2020-12-04 16:24:48 -0500

    Added hatch to entrance and exit

--- app.py
+++ app.py
@@ -7,15 +7,19 @@ Created on Thu Nov 19 06:58:24 2020.
 """
 import sys
 from PyQt5 import QtWidgets
-from numpy import zeros
+from numpy import zeros, arange, meshgrid, where
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.colors import LinearSegmentedColormap
 from random import randrange
 from random import seed
 
-from amazingmazes.mazes import Maze
-from amazingmazes.mazegui import Ui_mazeMenu
+try:
+    from amazingmazes.mazes import Maze
+    from amazingmazes.mazegui import Ui_mazeMenu
+except ModuleNotFoundError:  # Catch if 'amazingmazes' is not in path
+    from mazes import Maze
+    from mazegui import Ui_mazeMenu
 
 
 COLORMAP = LinearSegmentedColormap.from_list(
@@ -81,16 +85,27 @@ class App():
         return seed_
 
     def _drawMask(self, maze):
-        enterMask = (maze == 2)
-        exitMask = (maze == 3)
-        enterArray = zeros([enterMask.shape[0]+2, enterMask.shape[1]+2], dtype=int)
-        exitArray = zeros(exitMask.shape, dtype=int)
-        enterArray[1:-1, 1:-1][enterMask] = 1
-        exitArray[exitMask] = 1
+        j, i = where(maze == 2)
+        l, k = where(maze == 3)
+        i = i[0]
+        j = j[0]
+        k = k[0]
+        l = l[0]
+        diff = 0.99
 
-        
+        self._ax.fill(
+            [i, i+diff, i+diff, i],
+            [j, j, j+diff, j+diff],
+            fill=False,
+            hatch='////',
+            )
 
-        self._ax.contourf(enterArray, 1, hatches=['', '//'], alpha=0.5, origin=None)
+        self._ax.fill(
+            [k, k+diff, k+diff, k],
+            [l, l, l+diff, l+diff],
+            fill=False,
+            hatch='\\\\\\\\',
+            )
 
     def _drawMaze(self):
         self.ui.frame_2.setEnabled(False)
@@ -110,7 +125,7 @@ class App():
         self._ax = self.canvas.figure.subplots()
         self._ax.pcolormesh(maze, edgecolor=None, cmap=COLORMAP)
         self._drawMask(maze)
-#        self._ax.axis('off')
+        self._ax.axis('off')
         self._ax.set_facecolor('#000000')
         self.ui.drawButton.setText("Draw")
         self.ui.frame_2.setEnabled(True)
