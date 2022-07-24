825eab5c12c47fa3430bfeb4b7eb1c529d6bd35b
Robert Powel <48132667+rpowel@users.noreply.github.com>
2022-07-24 09:25:22 -0400

    fix __init__ formatting

--- app/mazes/__init__.py
+++ app/mazes/__init__.py
@@ -1,13 +1,13 @@
 from .base import MazeBase
-from .random_filled_maze import RandomMaze
-from .prim_maze import PrimMaze
 from .kruskal_maze import KruskalMaze
+from .prim_maze import PrimMaze
+from .random_filled_maze import RandomMaze
 from .recursive_division_maze import RecursiveDivisionMaze
 
 __all__ = [
-    MazeBase,
-    RandomMaze,
-    PrimMaze,
-    KruskalMaze,
-    RecursiveDivisionMaze,
+    "MazeBase",
+    "KruskalMaze",
+    "PrimMaze",
+    "RandomMaze",
+    "RecursiveDivisionMaze",
 ]
