07bc0d03020c4f43e5c7d52521a9814ddc57d3be
Robert Powel <48132667+rpowel@users.noreply.github.com>
2022-08-04 21:09:53 -0400

    add window size options

--- app/common/options/__init__.py
+++ app/common/options/__init__.py
@@ -1,8 +1,11 @@
 from .maze_sizes import MazeSizes
 from .maze_types import MazeTypes
+from .window_size import WindowHeight, WindowWidth
 
 
 __all__ = [
     "MazeSizes",
     "MazeTypes",
+    "WindowHeight",
+    "WindowWidth",
 ]
--- app/menus/options_menu.py
+++ app/menus/options_menu.py
@@ -2,6 +2,7 @@ import pygame
 from typing import Callable, List, Tuple
 
 from common.themes import THEME_LIST
+from common.options import WindowHeight, WindowWidth
 from processors import OptionsProcessor, RestartProcessor
 from .base import BaseMenu
 from .menu_objects import Button, Selector
@@ -12,11 +13,28 @@ class OptionsMenu(BaseMenu):
         super().__init__()
         self.window = window
 
+        self.theme_selector = Selector(
+            0.5, 0.1, THEME_LIST, current_value=self._theme.name, title="Color Theme"
+        )
+        self.changes = False
+
+        self.height_selector = Selector(
+            0.7,
+            0.25,
+            WindowHeight.list(),
+            current_value=self.window.get_height(),
+            title="Window Height",
+        )
+        self.width_selector = Selector(
+            0.3,
+            0.25,
+            WindowWidth.list(),
+            current_value=self.window.get_width(),
+            title="Window Width",
+        )
+
         back_img = pygame.image.load("images/arrow-left.png").convert_alpha()
         self.back_button = Button(0.25, 0.9, back_img)
-
-        self.theme_selector = Selector(0.5, 0.1, THEME_LIST, current_value=self._theme.name, title="Color Theme")
-        self.changes = False
 
         reset_img = pygame.image.load("images/recycle.png").convert_alpha()
         self.reset_button = Button(0.75, 0.9, reset_img)
@@ -33,16 +51,30 @@ class OptionsMenu(BaseMenu):
                 action = ""
             menu = "main"
 
-        theme_change, theme_selection = self.theme_selector.draw(self.window, event_list)
+        theme_change, theme_selection = self.theme_selector.draw(
+            self.window, event_list
+        )
         if theme_change:
             self.changes = True
             action = OptionsProcessor("display", "theme", theme_selection).process
-            menu = "options"
+
+        width_change, width_selection = self.width_selector.draw(
+            self.window, event_list,
+        )
+        if width_change:
+            self.changes = True
+            action = OptionsProcessor("display", "window_width", width_selection).process
+
+        height_change, height_selection = self.height_selector.draw(
+            self.window, event_list,
+        )
+        if height_change:
+            self.changes = True
+            action = OptionsProcessor("display", "window_height", height_selection).process
 
         if self.reset_button.draw(self.window, event_list):
             self._logger.info("reset button clicked")
             self.changes = True
             self._config.reset()
-            menu = "options"
 
         return action, menu
