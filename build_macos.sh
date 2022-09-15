deecf2aeb30093daa1851d1863705d35b786b50a
Robert Powel <48132667+rpowel@users.noreply.github.com>
2022-09-15 16:52:04 -0400

    make build script for Mac

---
+++ build_macos.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+version=$(cat ./app/VERSION.txt)
+
+pyinstaller ./app/app.py \
+  --name "Maze" \
+  --noconfirm \
+  --noconsole \
+  --add-data "./app/data/.gitkeep:data/.gitkeep" \
+  --add-data "./app/images/*:images" \
+  --add-data "./app/resources/default_config.cfg:resources" \
+  --icon "./app/images/icon.png" \
+  --clean \
+
+retval=$?
+
+if [ $retval -ne 0 ]; then
+  echo "Build exited with error code: "$retval
+  exit
+fi
+
+cd dist || exit
+zip -9Tr ./Maze_macos_"$version".zip ./Maze.app
+cd .. || exit
+
+./dist/Maze/Maze
