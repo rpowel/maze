0e9dbdbdff192e6dac118edcefccca483dbe07a3
Robert Powel <48132667+rpowel@users.noreply.github.com>
2022-09-16 11:22:58 -0400

    move tests

--- tests/test_kruskal_maze.py
+++ tests/test_kruskal_maze.py
@@ -5,14 +5,14 @@ import numpy as np
 from mazes.maze_generators import KruskalMaze
 
 
-def test_make_maze():
+def test_generate():
     random.seed(1)
     n_x = n_y = 10
-    result = [val for val in KruskalMaze().generate(n_x, n_y)][0]
+    result = [val for val in KruskalMaze().generate(n_x, n_y)][-1]
     correct = np.array(
         [
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+            [2, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 3],
             [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1],
             [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
             [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1],
