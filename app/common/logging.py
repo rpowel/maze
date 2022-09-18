8c6dae17f94a12ef0fbe4209ab3ae3e0ac82dfe8
Robert Powel <48132667+rpowel@users.noreply.github.com>
2022-09-18 14:22:56 -0400

    test change

--- app/common/logging.py
+++ app/common/logging.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="[%(asctime)s %(name)s %(levelname)s] %(message)s",
+    format="[%(asctime)s %(name)s  %(levelname)s] %(message)s",
 )
 
 
